55afdcd062cf3c8bdc3465ea1070bbda0a002fdd
f1kusss <obikfikus@gmail.com>
2023-04-13 18:22:02 +0300

    RoomPlacer

---
+++ Rogue/Assets/Room.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject DoorU;
+    public GameObject DoorR;
+    public GameObject DoorD;
+    public GameObject DoorL;
+
+    public Mesh[] BlockMeshes;
+
+    public void RotateRandomly()
+    {
+        int count = Random.Range(0, 4);
+
+        for (int i = 0; i < count; i++)
+        {
+            transform.Rotate(0, 90, 0);
+
+            GameObject tmp = DoorL;
+            DoorL = DoorD;
+            DoorD = DoorR;
+            DoorR = DoorU;
+            DoorU = tmp;
+        }
+    }
+}
